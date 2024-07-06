778d2956620f1939b8843c840e2d7fc642fddeda
Rednowski <kuba.rednowski@gmail.com>
2024-07-06 19:25:47 +0200

    initial spring files, logo

---
+++ src/main/java/pl/edu/wszib/game/store/config/AppConfiguration.java
@@ -0,0 +1,9 @@
+package pl.edu.wszib.game.store.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("pl.edu.wszib.game.store")
+public class AppConfiguration {
+}
